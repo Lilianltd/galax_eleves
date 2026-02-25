87653f9bc8379ed719f3f0321d724d7429b3ae16
Lilian LETARD <lilian.letard@gmail.com>
2026-02-25 16:43:54 +0100

    v1 gpu

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -7,12 +7,49 @@
 
 __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i >= n_particles){
+		return;
+	}
+	accelerationsGPU[i] = make_float3(0.0f, 0.0f, 0.0f);
+		
+	for (int j = 0; j < n_particles; j++)
+		{
+			const float diffx = positionsGPU[j].x - positionsGPU[i].x;
+			const float diffy = positionsGPU[j].y - positionsGPU[i].y;
+			const float diffz = positionsGPU[j].z - positionsGPU[i].z;
+
+			float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+			if (dij < 1.0)
+			{
+				dij = 10.0;
+			}
+			else
+			{
+				dij = std::sqrt(dij);
+				dij = 10.0 / (dij * dij * dij);
+			}
+
+			accelerationsGPU[i].x += diffx * dij * massesGPU[j];
+			accelerationsGPU[i].y += diffy * dij * massesGPU[j];
+			accelerationsGPU[i].z += diffz * dij * massesGPU[j];
+		}
 }
+
 
 __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, int n_particles)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i >= n_particles){
+		return;
+	}
+	velocitiesGPU[i].x += accelerationsGPU[i].x * 2.0f;
+	velocitiesGPU[i].y += accelerationsGPU[i].y * 2.0f;
+	velocitiesGPU[i].z += accelerationsGPU[i].z * 2.0f;
+	positionsGPU[i].x += velocitiesGPU[i].x * 0.1f;
+	positionsGPU[i].y += velocitiesGPU[i].y * 0.1f;
+	positionsGPU[i].z += velocitiesGPU[i].z * 0.1f;
 
 }
 
@@ -26,4 +63,4 @@ void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* acc
 }
 
 
-#endif // GALAX_MODEL_GPU
+#endif
